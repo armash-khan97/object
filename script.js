c6d81afe764c8dbdbd836401e8f83b4f569691b3
Armash khan <79808235+armash-khan97@users.noreply.github.com>
2021-07-25 12:08:29 +0500

    ok

---
+++ script.js
@@ -0,0 +1,65 @@
+// question 01
+// var itemsArray =[{
+//         name:"juice",
+//         price:"50",
+//         quantity:"3",
+// },{
+//         name:"cookie",
+//         price:"30",
+//         quantity:"9",
+// },{
+//         name:"shirt",
+//         price:"880",
+//         quantity:"1",
+// },{
+//         name:"pen",
+//         price:"100",
+//         quantity:"2",
+// },]
+
+// var total1 = (itemsArray[0].price*itemsArray[0].quantity);
+// var total2 = (itemsArray[1].price*itemsArray[1].quantity);
+// var total3 = (itemsArray[2].price*itemsArray[2].quantity);
+// var total4 = (itemsArray[3].price*itemsArray[3].quantity);
+
+// var total =(total1+total2+total3+total4);
+// console.log(total1);
+// console.log(total2);
+// console.log(total3);
+// console.log(total4);
+// console.log(total);
+
+
+// question 02
+
+
+// var persons ={
+//     name:"armash",
+//     email:"armashbhaijan97@gmail.com",
+//     password:"090076601",
+//     age:"23",
+//     gender:"male",
+//     city:"karachi",
+//     country:"pakistan",
+// }
+// console.log("country" in persons);
+// console.log("age" in persons);
+// console.log("firstName" in persons);
+// console.log("lastName" in persons );
+
+
+// question 3
+
+// function Student(name, rollNo, batch, email, course) {
+//     this.name = name;
+//     this.rollNo = rollNo;
+//     this.batch = batch;
+//     this.email = email;
+//     this.course = course;
+// }
+        
+// var Student1 = new Student("Armash", 21693, 07, "arm@.com", "web & mobileApp");
+// var Student2 = new Student("Asad", 21675, 07, "asad@.com", "web & mobileApp");
+// var Student3 = new Student("Basit", 21290, 07, "basit@.com", "web & mobileApp");
+// var Student4 = new Student("Ali", 21230, 07, "ali@.com", "web & mobileApp");
+// console.log(Student1, Student2, Student3, Student4);
